ab37880a4f72cebf17d0224eb8efaa35dee96639
dsmolina <79925931+dsmolina@users.noreply.github.com>
2023-03-22 18:14:41 -0400

    Add files via upload

---
+++ cprocess.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+
+int main()
+{
+	pid_t c_pid = fork();
+
+	if (c_pid == -1) {
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (c_pid > 0) {
+		// wait(nullptr);
+		cout << "printed from parent process " << getpid()
+			<< endl;
+	}
+	else {
+		cout << "printed from child process " << getpid()
+			<< endl;
+	}
+
+	return 0;
+}
